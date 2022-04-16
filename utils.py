6140d6178ff87087f282b0f5f60eb2cd744e7513
JessicaChildress <jchildress@zagmail.gonzaga.edu>
2022-04-15 22:27:13 -0700

    weird underlines and stuff

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 
+from re import M
 import pandas as pd
 
 
@@ -26,7 +27,7 @@ def sec_to_hours(seconds):
     b=str((seconds%3600)//60)
     c=str((seconds%3600)%60)
     d=["{} hours {} mins {} seconds".format(a, b, c)]
-    return a,b,c,d
+    return d
 
 # this function allocates a series for each day of the week
 # note: uses the 'fromDate' column to mark when sleep started
@@ -44,31 +45,46 @@ def separate_days(df):
         curr_day = ser.iloc[i]
         mon_check = ser.str.contains('Monday', case=False, na=False)
         if mon_check.iloc[i] == True:
-            mon.append(curr_day)
+            mon.append(df["asleep"].iloc[i])
         tues_check = ser.str.contains('Tuesday', case=False, na=False)
         if tues_check.iloc[i] == True:
-            tues.append(curr_day)
+            tues.append(df["asleep"].iloc[i])
         wed_check = ser.str.contains('Wednesday', case=False, na=False)
         if wed_check.iloc[i] == True:
-            wed.append(curr_day)
+            wed.append(df["asleep"].iloc[i])
         thurs_check = ser.str.contains('Thursday', case=False, na=False)
         if thurs_check.iloc[i] == True:
-            thurs.append(curr_day)
+            thurs.append(df["asleep"].iloc[i])
         fri_check = ser.str.contains('Friday', case=False, na=False)
         if fri_check.iloc[i] == True:
-            fri.append(curr_day)
+            fri.append(df["asleep"].iloc[i])
         sat_check = ser.str.contains('Saturday', case=False, na=False)
         if sat_check.iloc[i] == True:
-            sat.append(curr_day)
+            sat.append(df["asleep"].iloc[i])
         sun_check = ser.str.contains('Sunday', case=False, na=False)
         if sun_check.iloc[i] == True:
-            sun.append(curr_day)
-        mon = pd.Series(mon)
-        tues = pd.Series(tues)
-        wed = pd.Series(wed)
-        thurs = pd.Series(thurs)
-        fri = pd.Series(fri)
-        sat = pd.Series(sat)
-        sun = pd.Series(sun)
-    return mon, tues, wed, thurs, fri, sat, sun
-    
+            sun.append(df["asleep"].iloc[i])
+    mon = pd.Series(mon)
+    tues = pd.Series(tues)
+    wed = pd.Series(wed)
+    thurs = pd.Series(thurs)
+    fri = pd.Series(fri)
+    sat = pd.Series(sat)
+    sun = pd.Series(sun)
+    week = {'Monday':mon, 'Tuesday':tues, 'Wednesday':wed, 'Thursday':thurs, 'Friday':fri, 'Saturday':sat, 'Sunday':sun }
+    return week
+
+
+    # this function also works with each day of the week in individual series
+    # the seconds of sleep will be rounded to the nearest hundredth of an hour
+    # and then each day of the week's series will be evaluated to determine
+    # which day of the week has the most occurences of the mode value (8.78)
+    def mode_by_day(df):
+        M, T, W, R, F, Sa, Su = ([], ) * 7
+        curr_col = df["Monday"]
+        for i in range(len(df["Monday"])):
+            curr_day = df["Monday"].iloc[i]
+            hours = round(df["Monday"].iloc[i]/3600, 2)
+            M[i] = hours
+        print(M)
+        return M
